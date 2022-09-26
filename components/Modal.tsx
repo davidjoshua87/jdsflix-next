eed6df2947145fa1987b58c4e639cf05f9b34f81
davidjoshua87 <davidjosh_87@yahoo.com>
2022-09-27 02:43:18 +0700

    fix: #2 edit modal play video.

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,5 @@
-import { XMarkIcon } from '@heroicons/react/24/solid';
-import { SpeakerWaveIcon, SpeakerXMarkIcon } from '@heroicons/react/24/solid';
+import { XMarkIcon, SpeakerWaveIcon, SpeakerXMarkIcon, PlayCircleIcon, PauseCircleIcon } from '@heroicons/react/24/solid';
+import {  } from '@heroicons/react/24/solid';
 import MuiModal from '@mui/material/Modal';
 import { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
@@ -68,13 +68,13 @@ export default function Modal() {
     <MuiModal
       open={showModal}
       onClose={handleClose}
-      className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+      className="fixed !top-20 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
     >
       <>
         <Toaster position="bottom-center" />
         <button
           onClick={handleClose}
-          className="modalButton absolute right-5 top-5 z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+          className="modalButton absolute right-5 lg:top-20 md:top-6 z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#333]"
         >
           <XMarkIcon className="h-6 w-6" />
         </button>
@@ -88,22 +88,15 @@ export default function Modal() {
             playing={pause}
             muted={muted}
           />
-          <div className="absolute bottom-20 flex w-full items-center justify-between px-10">
+          <div className="absolute bottom-5 lg:bottom-20 md:bottom-10 flex w-full items-center justify-between px-10">
             <div className="flex space-x-3">
-              <button onClick={() => setPause(!muted)} className="flex items-center gap-x-2 rounded bg-white px-8 pt-2 pb-2 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
-                <FaPlay className="h-7 w-7 text-black" />
-                Play
-              </button>
-              {/* <button className="modalButton">
-                {addedToList ? (
-                  <CheckIcon className="h-7 w-7" />
+              <button onClick={() => setPause(!pause)} className="flex items-center gap-x-2 rounded-xl bg-white px-2 pt-1 pb-1 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                {pause ? (
+                  <PlayCircleIcon className="h-7 w-7 text-black" /> 
                 ) : (
-                  <PlusIcon className="h-7 w-7" />
+                  <PauseCircleIcon className="h-7 w-7 text-black" />
                 )}
               </button>
-              <button className="modalButton cursor-not-allowed">
-                <ThumbUpIcon className="h-7 w-7" />
-              </button> */}
             </div>
             <button onClick={() => setMuted(!muted)}>
               {muted ? (
@@ -114,41 +107,6 @@ export default function Modal() {
             </button>
           </div>
         </div>
-
-        {/* <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
-          <div className="space-y-6 text-lg">
-            <div className="flex items-center space-x-2 text-sm">
-              <p className="font-semibold text-green-400">
-                {(movie?.vote_average || 0) * 10}% Match
-              </p>
-              <p className="font-light">
-                {movie?.release_date || movie?.first_air_date}
-              </p>
-              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
-                HD
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
-              <p className="w-5/6">{movie?.overview}</p>
-              <div className="flex flex-col space-y-3 text-sm">
-                <div>
-                  <span className="text-[gray]">Genres: </span>
-                  {genres.map((genre) => genre.name).join(', ')}
-                </div>
-                <div>
-                  <span className="text-[gray]">Original language: </span>
-                  {movie?.original_language}
-                </div>
-
-                <div>
-                  <span className="text-[gray]">Total votes: </span>
-                  {movie?.vote_count}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </>
     </MuiModal>
   );
